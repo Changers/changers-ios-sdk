334af4e9f5ab1c2425afae43d040add4fada6f1c
Clément YEROCHEWSKI <yerochewski@gmail.com>
2020-09-30 14:36:44 +0200

    remove unneeded folders

--- derived_data/Build/Intermediates.noindex/changers-sdk.build/Release-iphoneos/ChangersSDK.build/Objects-normal/arm64/ChangersSDK_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/cyerochewski/Workspace/Changers-iOS-SDK/SDK/changers-ios-sdk/derived_data/Build/Intermediates.noindex/changers-sdk.build/Release-iphoneos/ChangersSDK.build/DerivedSources/ChangersSDK_vers.c
